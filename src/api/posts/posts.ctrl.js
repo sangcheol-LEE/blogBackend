8234c0c12ad200f94596ea18fbf3a744eb0678e9
이상철 <zlrz6706@gmail.com>
2022-09-01 21:42:09 +0900

    백엔드 맛보기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -23,3 +23,100 @@ exports.write = ctx => {
   posts.push(post);
   ctx.body = post
 };
+
+/* 포스트 목록 조회
+  GET / api/posts
+ */
+exports.list = ctx => {
+  ctx.body = posts;
+};
+
+// 특정 포스트 조회
+// GET /api/posts/:id
+exports.read = ctx => {
+  const { id } = ctx.params;
+  // 주어진 id 값으로 포스트를 찾습니다.
+  // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
+  // 비교할 p.id 값을 문자열로 변경해야 합니다.
+
+  const post = posts.find(p => p.id.toString() === id);
+  // 포스트가 없으면 오류를 반환합니다.
+  if(!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 존재하지 않습니다."
+    };
+    return ;
+  }
+  ctx.body = post;
+};
+
+// 특정 포스트 제거
+// DELETE /api/posts/:id
+exports.remove = ctx => {
+  const { id } = ctx.params;
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+  const index = posts.findIndex(p => p.id.toString() === id);
+  // 포스트가 없으면 오류를 반환합니다.
+  if(index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message : "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+  // index번째 아이템을 제거합니다.
+  posts.splice(index, 1);
+  ctx.status = 204; // No Content
+};
+
+// 포스트 수정(교체)
+// PUT /api/posts/:id
+// {title, body}
+exports.replace = ctx => {
+  // PUT 메서드는 전체 포스트 정보를 입력하여 데잍터를 통째로 교체할 때 사용합니다.
+  const { id } = ctx.params;
+  // 해당 id를 가진 post가 몇번 째 인지 확인합니다.
+  const index = posts.findIndex(p => p.id.toString() === id);
+  // 포스트가 없으면 오류를 반환합니다.
+  if(index === -1) {
+    ctx.status = 404;
+    ctx.body ={
+      message : "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+
+  // 전체 객체를 덮어 씌웁니다.
+  // 따리서 id를 제외한 기본정보를 날리고, 객체를 새로 만듭니다.
+  posts[index] ={
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+// 포스트 수정(특정 필드 변경)
+// PATCH /api/posts/:id
+// {title, body}
+
+exports.update = ctx => {
+  // PATCH 메서드는 주어진 필드만 교체합니다.
+  const { id } = ctx.params;
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+  const index = posts.findIndex(p => p.id.toString() === id);
+  // 포스트가 없으면 오류를 반환합니다.
+  if(index === -1) {
+    ctx.status = 404
+    ctx.body = {
+      message : "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+  // 기존 값에 정보를 덮어 씌웁니다.
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+}
