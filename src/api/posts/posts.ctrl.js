07643f93726b04e72975c9c861e99f5ce06b5ae6
이상철 <zlrz6706@gmail.com>
2022-09-02 22:37:58 +0900

    몽고 디비를 연동한 REST API

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -37,8 +37,57 @@ export const list = async ctx => {
   }
 }
 
-export const read = ctx => {}
+/* 
+  GET /api/ posts/:id
+*/
+export const read = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if(!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch(e) {
+    ctx.throw(500,e)
+  }
+}
 
+/* 
+  DELETE /api/posts/:id
+*/
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No connect ("성공은 했지만 응답할 데이터는 없음")
+  } catch(e) {
+    ctx.throw(500,e)
+  }
+}
 
-export const remove = ctx => {}
-export const update = ctx => {}
+/* 
+  PATCH / api/posts/:id
+  {
+    title : "수정 ",
+    body: "수정 내용 ",
+    tags: ['수정','태그']
+  }
+*/
+export const update = async ctx => {
+  const {id} = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new :true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false일 때는 업데이트되기 전의 데이터를 반환합니다.
+    }).exec();
+    if(!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.status = 404;
+  }catch(e) {
+    ctx.throw(500, e);
+  }
+}
